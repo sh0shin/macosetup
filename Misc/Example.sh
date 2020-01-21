a92af7314ac50c010241657c3a820b93ffcf34da
Chris 'sh0shin' Frage <git@sh0shin.org>
2020-01-21 10:56:36 +0100

    Example script

---
+++ Misc/Example.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#
+
+# Don't run directly!
+[[ "$_" == "$0" ]] && exit 255
+
+# Example
+[[ "${_example_enabled:-false}" == false ]] && return
+echo "${BASH_SOURCE[0]}"
+
+# ...
+
+# vim: set syn=bash sw=2 ts=2 et :
+# eof
