7ceb3c1fb6d39b5d8f69518c4ee98adfa375416d
Chris 'sh0shin' Frage <git@sh0shin.org>
2020-01-21 10:52:52 +0100

    Setup functions and run

--- Setup.sh
+++ Setup.sh
@@ -4,5 +4,68 @@
 # macOSetup
 #
 
-# vim: set syn=sh sw=2 ts=2 et :
+# Variables
+export _user="$(whoami)"
+export _fullname="$(finger ${_user} | awk '/Name:/ { print $4 }')"
+
+# Functions
+_usage()
+{
+  echo "Usage: $0 <config-file> [run-script(s)]"
+  exit 1
+}
+
+_osupport()
+{
+  if [[ $OSTYPE != darwin* ]]
+  then
+    echo "Only Darwin/macOS is supported!"
+    exit 1
+  fi
+}
+
+_main()
+{
+  _osupport
+
+  _config="$1"
+
+  if [[ -e "$_config" ]]
+  then
+    source "$_config"
+    _local="$(basename "$_config")"
+    shift
+  else
+    echo "$_config not found!"
+    exit 1
+  fi
+
+  # Run selected only
+  if [[ $# -gt 0 ]]
+  then
+    for _run in $@
+    do
+      test -x "$_run" && source "$_run"
+      exit $?
+    done
+  fi
+
+  # Run all
+  for _run in Shell/*.sh Homebrew/*.sh SystemPreferences/*.sh Applications/*.sh Misc/*.sh
+  do
+    test -x "$_run" && source "$_run"
+  done
+
+  # Local
+  test -x "Local.d/$_local" && source "Local.d/$_local"
+}
+
+if [[ $# -lt 1 ]]
+then
+  _usage
+else
+  _main "$@"
+fi
+
+# vim: set syn=bash sw=2 ts=2 et :
 # eof
