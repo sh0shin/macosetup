1da87fe465cc8e82160c86571ebc2ee3f4c9155e
Chris 'sh0shin' Frage <git@sh0shin.org>
2020-06-05 12:48:25 +0200

    ToudID notes

--- SystemPreferences/TouchID.sh
+++ SystemPreferences/TouchID.sh
@@ -8,7 +8,20 @@
 [[ "${_touch_id_enabled:-false}" == false ]] && return
 echo "${BASH_SOURCE[0]}"
 
-# ...
+# Use Touch ID for:
+
+# Unlocking your MAC
+#???
+# Apple Pay
+#???
+
+# iTunes Store, App Store & Apple Books
+#defaults write com.apple.AppleMediaServices AMSDeviceBiometricsState -int 2
+#defaults write com.apple.AppleMediaServices AMSDeviceBiometricsState -int 1
+
+# Password AutoFill
+#defaults write com.apple.Safari TouchIDToAutoFill -bool false
+#defaults write com.apple.Safari TouchIDToAutoFill -bool true
 
 # vim: set syn=bash sw=2 ts=2 et :
 # eof
