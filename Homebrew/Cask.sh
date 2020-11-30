5688c5649c4614c1234f3689ed912317d3123cef
Chris 'sh0shin' Frage <git@sh0shin.org>
2020-11-30 21:11:47 +0100

    Syntax fixes

--- Homebrew/Cask.sh
+++ Homebrew/Cask.sh
@@ -14,13 +14,13 @@ echo "${BASH_SOURCE[0]}"
 brew tap homebrew/cask
 
 # Tapping cask taps
-for _tap in "${_brew_cask_taps[@]}"
+for _tap in "${_brew_cask_taps[@]:-()}"
 do
   brew tap "$_tap"
 done
 
 # Install Casks
-if [[ -n "${_brew_casks[@]}" ]]
+if [[ -n "${_brew_casks[*]:-()}" ]]
 then
 
   for _cask in "${_brew_casks[@]}"
@@ -39,7 +39,7 @@ then
 fi
 
 # Cleanup
-[[ ${_brew_cleanup:-false} == true ]] && brew cleanup --prune=${_brew_cleanup_prune:-0}
+[[ ${_brew_cleanup:-false} == true ]] && brew cleanup --prune="${_brew_cleanup_prune:-0}"
 
-# vim: set syn=bash sw=2 ts=2 et :
+# vim: set ft=sh syn=sh sw=2 ts=2 et :
 # eof
