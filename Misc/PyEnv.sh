7622f7a69f35d0db10ddce689e1dc08ec10689fb
Chris 'sh0shin' Frage <git@sh0shin.org>
2020-12-02 07:57:23 +0100

    Style & TODO...

--- Misc/PyEnv.sh
+++ Misc/PyEnv.sh
@@ -8,6 +8,9 @@
 [[ "${_pyenv_enabled:-false}" == false ]] && return
 echo "${BASH_SOURCE[0]}"
 
+
+# TODO:
+# Use homebrew if available
 export PYENV_ROOT="${_pyenv_root:-$HOME/.pyenv}"
 export PATH="$PYENV_ROOT/bin:$PATH"
 
@@ -16,7 +19,7 @@ if [[ -d "$PYENV_ROOT" ]]
 then
   ( cd "$PYENV_ROOT" && git pull )
 else
-  git clone "${_pyenv_git:-https://github.com/pyenv/pyenv.git}" "$PYENV_ROOT"
+  git clone "${_pyenv_git_url:-https://github.com/pyenv/pyenv.git}" "$PYENV_ROOT"
 fi
 eval "$(pyenv init -)"
 
@@ -27,20 +30,20 @@ then
   then
     ( cd "$PYENV_ROOT/plugins/pyenv-virtualenv" && git pull )
   else
-    git clone "${_pyenv_venv_git:-https://github.com/pyenv/pyenv-virtualenv.git}" "$PYENV_ROOT/plugins/pyenv-virtualenv"
+    git clone "${_pyenv_venv_git_url:-https://github.com/pyenv/pyenv-virtualenv.git}" "$PYENV_ROOT/plugins/pyenv-virtualenv"
   fi
   eval "$(pyenv virtualenv-init -)"
 fi
 
 # versions
-if [[ -n "${_pyenv_versions[@]}" ]]
+if [[ -n "${_pyenv_versions[*]:-()}" ]]
 then
   for _version in "${_pyenv_versions[@]}"
   do
-    echo $_version
+    echo "$_version"
     pyenv install -s "$_version"
   done
 fi
 
-# vim: set syn=bash sw=2 ts=2 et :
+# vim: set ft=sh syn=sh sw=2 ts=2 et :
 # eof
