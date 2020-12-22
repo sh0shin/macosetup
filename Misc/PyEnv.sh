134a47224c526c5bfb9d827fd14358b50edb0cb4
Chris 'sh0shin' Frage <git@sh0shin.org>
2020-12-22 18:41:49 +0100

    Use pyenv from brew (if installed)

--- Misc/PyEnv.sh
+++ Misc/PyEnv.sh
@@ -8,30 +8,45 @@
 [[ "${_pyenv_enabled:-false}" == false ]] && return
 echo "${BASH_SOURCE[0]}"
 
-
-# TODO:
-# Use homebrew if available
 export PYENV_ROOT="${_pyenv_root:-$HOME/.pyenv}"
 export PATH="$PYENV_ROOT/bin:$PATH"
 
-# git clone/pull
-if [[ -d "$PYENV_ROOT" ]]
+# pyenv from git
+if [[ "${_pyenv_git_enabled:-false}" == true ]]
 then
-  ( cd "$PYENV_ROOT" && git pull )
-else
-  git clone "${_pyenv_git_url:-https://github.com/pyenv/pyenv.git}" "$PYENV_ROOT"
+  if [[ -d "$PYENV_ROOT/.git" ]]
+  then
+    ( cd "$PYENV_ROOT" && echo git pull )
+  else
+    echo git clone "${_pyenv_git_url:-https://github.com/pyenv/pyenv.git}" "$PYENV_ROOT"
+  fi
 fi
-eval "$(pyenv init -)"
 
-# virtualenv
+# init
+_pyenv_bin="$(which pyenv)"
+if [[ -n "$_pyenv_bin" ]]
+then
+  eval "$(pyenv init -)"
+fi
+
+# virtualenv from git
 if [[ "${_pyenv_venv_enabled:-false}" == true ]]
 then
-  if [[ -d "$PYENV_ROOT/plugins/pyenv-virtualenv" ]]
+  if [[ "${_pyenv_venv_git_enabled:-false}" == true ]]
   then
-    ( cd "$PYENV_ROOT/plugins/pyenv-virtualenv" && git pull )
-  else
-    git clone "${_pyenv_venv_git_url:-https://github.com/pyenv/pyenv-virtualenv.git}" "$PYENV_ROOT/plugins/pyenv-virtualenv"
+    if [[ -d "$PYENV_ROOT/plugins/pyenv-virtualenv/.git" ]]
+    then
+      ( cd "$PYENV_ROOT/plugins/pyenv-virtualenv" && echo git pull )
+    else
+      echo git clone "${_pyenv_venv_git_url:-https://github.com/pyenv/pyenv-virtualenv.git}" "$PYENV_ROOT/plugins/pyenv-virtualenv"
+    fi
   fi
+fi
+
+# init
+_pyenv_venv_bin="$(which pyenv-virtualenv)"
+if [[ -n "$_pyenv_venv_bin" ]]
+then
   eval "$(pyenv virtualenv-init -)"
 fi
 
@@ -40,7 +55,7 @@ if [[ -n "${_pyenv_versions[*]:-()}" ]]
 then
   for _version in "${_pyenv_versions[@]}"
   do
-    echo "$_version"
+    echo "pyenv version $_version"
     pyenv install -s "$_version"
   done
 fi
