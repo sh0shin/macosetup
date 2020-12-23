51fb632efbf55b63948ad9e009363479a4b374bd
Chris 'sh0shin' Frage <git@sh0shin.org>
2020-12-23 13:05:21 +0100

    New naming convention import

---
+++ shell/sysctl.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#
+
+# Don't run directly!
+[[ "$_" == "$0" ]] && exit 255
+
+# Sysctl
+[[ "${_sysctl_enabled:-false}" == false ]] && return
+echo "${BASH_SOURCE[0]}"
+
+# Config
+IFS=$'\n'
+echo "echo \"${_sysctl_flags[*]:-()}\" >/private/etc/sysctl.conf" | sudo -s
+unset IFS
+
+# Set
+for _flag in "${_sysctl_flags[@]}"
+do
+  sudo sysctl "$_flag"
+done
---
+++ shell/xcode.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#
+
+# Don't run directly!
+[[ "$_" == "$0" ]] && exit 255
+
+# Xcode
+[[ "${_xcode_enabled:-true}" == false ]] && return
+echo "${BASH_SOURCE[0]}"
+
+# Install Xcode Command Line Tools
+if ! xcode-select -p &>/dev/null
+then
+  xcode-select --install 2>/dev/null
+
+  # Wait for installation to finish
+  until xcode-select -p &>/dev/null
+  do
+    sleep 5
+  done
+fi
