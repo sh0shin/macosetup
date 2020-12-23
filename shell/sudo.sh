9777a8b874cbb3134774711bc7ce7a360c6fd87b
Chris 'sh0shin' Frage <git@sh0shin.org>
2020-12-23 13:21:54 +0100

    Use new naming convention

--- shell/sudo.sh
+++ shell/sudo.sh
@@ -8,5 +8,5 @@
 [[ "${_sudo_enabled:-true}" == false ]] && return
 echo "${BASH_SOURCE[0]}"
 
-sudo cp "File.d/${_sudo_file:-sudo.80admin.in}" "${_sudo_path:-/private/etc/sudoers.d}/${_sudo_dest:-80admin}"
+sudo cp "file.d/${_sudo_file:-sudo.80admin.in}" "${_sudo_path:-/private/etc/sudoers.d}/${_sudo_dest:-80admin}"
 sudo chmod 0440 "${_sudo_path:-/private/etc/sudoers.d}/${_sudo_dest:-80admin}"
